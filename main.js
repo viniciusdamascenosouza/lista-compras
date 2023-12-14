7dd2308ed94c562bdbe48e02bd175ac9b78e2afe
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-14 17:18:25 -0300

    Checkbox

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ let listaDeItens = [];
 const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
 const ulItens = document.getElementById("lista-de-itens");
+const ulItensComprados = document.getElementById("itens-comprados");
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
 
   salvarItem();
-
   mostrarItens();
+  itensInput.focus();
 });
 
 function salvarItem() {
@@ -24,23 +25,38 @@ function salvarItem() {
     listaDeItens.push({ valor: comprasItem });
   }
 
-  console.log(listaDeItens);
+  itensInput.value = "";
 }
 
 function mostrarItens() {
   ulItens.innerHTML = "";
+  ulItensComprados.innerHTML = "";
   listaDeItens.forEach((elemento, index) => {
-    ulItens.innerHTML += `
-        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-            <div>
-                <input type="checkbox" class="is-clickable" />
-                <input type="text" class="is-size-5" value="${elemento.valor}"></input>
-            </div>
-            <div>
-                <i class="fa-solid fa-trash is-clickable deletar"></i>
-            </div>
-        </li>
-    `;
+    if (elemento.checar) {
+      ulItensComprados.innerHTML += `
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" checked class="is-clickable" />  
+                    <span class="itens-comprados is-size-5">${elemento.valor}</span>
+                </div>
+                <div>
+                    <i class="fa-solid fa-trash is-clickable deletar"></i>
+                </div>
+            </li>
+        `;
+    } else {
+      ulItens.innerHTML += `
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" class="is-clickable" />
+                    <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+                </div>
+                <div>
+                    <i class="fa-solid fa-trash is-clickable deletar"></i>
+                </div>
+            </li>
+        `;
+    }
   });
 
   const inputsCheck = document.querySelectorAll('input[type="checkbox"]');
@@ -50,7 +66,7 @@ function mostrarItens() {
       const valorDoElemento =
         evento.target.parentElement.parentElement.getAttribute("data-value");
       listaDeItens[valorDoElemento].checar = evento.target.checked;
-      console.log(listaDeItens[valorDoElemento].checar);
+      mostrarItens();
     });
   });
 }
