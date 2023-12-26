1948fe6f65bf9c7a98f72627c9d00137bc14d3a0
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-26 08:16:04 -0300

    Editando no DOM

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function mostrarItens() {
                     <input type="text" class="is-size-5" value="${elemento.valor}"></input>
                 </div>
                 <div>
-                    <i class="fa-regular fa-floppy-disk is-clickable"></i><i class="fa-regular is-clickable
+                    <button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button> <i class="fa-regular is-clickable
                       fa-pen-to-square editar">
                     </i>
                     <i class="fa-solid fa-trash is-clickable deletar"></i>
@@ -90,10 +90,16 @@ function mostrarItens() {
 
   editarItens.forEach((i) => {
     i.addEventListener("click", (evento) => {
-      const itemAEditar =
+      itemAEditar =
         evento.target.parentElement.parentElement.getAttribute("data-value");
       mostrarItens();
-      console.log(itemAEditar);
     });
   });
+}
+
+function salvarEdicao() {
+  const itemEditado = document.querySelector(
+    `[data-value="${itemAEditar}"] input[type="text"]`
+  );
+  console.log(itemEditado.value);
 }
